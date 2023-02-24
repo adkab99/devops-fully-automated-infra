bc104771801257eb94c4163d9c48a5e5e75ff65c
abdulkabir adejumo <adkab99@gmail.com>
2023-02-23 18:48:51 -0600

    ade

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -71,7 +71,7 @@ resource "aws_instance" "web_server" {
 
   # best practices as per checkov scanner
 
-  # monitoring = true
+  monitoring = true
   # # ebs_optimized = true
   # root_block_device {
   # encrypted     = true
